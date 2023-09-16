4f7bce1316deb7b83838923539e61b311e225a81
engageintellect <jc9361@gmail.com>
2023-09-15 21:21:07 -0700

    init2

--- pages/api/[...route].ts
+++ pages/api/[...route].ts
@@ -1,15 +1,11 @@
 import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-
-export const config = {
-  runtime: 'edge'
-}
+import { handle } from '@hono/node-server/vercel'
 
 const app = new Hono().basePath('/api')
 
 app.get('/hello', (c) => {
   return c.json({
-    message: 'Hello from Hono!'
+    message: 'Hello from Hono!',
   })
 })
 
